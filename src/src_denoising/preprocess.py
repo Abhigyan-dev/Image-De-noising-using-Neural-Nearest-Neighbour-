bf2459648352c15529ee1b1560f23dbca710976a
AbhigyanGargav <abhigyangargava@gmail.com>
2021-12-07 09:08:17 +0530

    removing src

--- src/src_denoising/preprocess.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-class RandomOrientation90(object):
-    def __call__(self, img):
-        degrees = 90*np.random.randint(0, 4)
-        img.rotate(degrees)
-        return img
